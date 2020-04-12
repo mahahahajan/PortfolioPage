0993027a15b0db3b1684e80fcbcf50a5cbd97020
mahahahajan <pulkit8.mahajan@gmail.com>
2020-04-12 15:46:31 -0700

    Finished  for now

---
+++ main.js
@@ -0,0 +1,6 @@
+// let root = document.documentElement;
+
+// root.addEventListener("mousemove", e => {
+//   root.style.setProperty('--background', "black");
+//   root.style.setProperty('--text', "white");
+// });
